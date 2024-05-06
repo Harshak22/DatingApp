5ebf274ebb90baf55daafefa58da966098c5bb24
Harshak22 <harshakallumkuzhiyil@gmail.com>
2024-05-06 23:28:24 +0530

    End of chapter 1

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Entity;
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<AppUser>Users{ get; set; }
+}
